74e0bcabf8bd38c89debfc6dc81621b23e5038c1
ishan-modi <ishan.modi24@gmail.com>
2023-08-01 17:29:54 +0530

    minor updates

--- preprocess/preprocess_sroie.py
+++ preprocess/preprocess_sroie.py
@@ -5,9 +5,10 @@ from dataclasses import dataclass, field
 from tqdm import tqdm
 import jellyfish
 
+
 @dataclass
 class PreprocessorSROIE:
-    processed_data: dict = field(default_factory=dict)
+    processed_data: list = field(default_factory=list)
     process_mode: str = 'train'
     img_path: str = 'img/'
     entities_path: str = 'entities/'
@@ -21,9 +22,10 @@ class PreprocessorSROIE:
             self.base_path = '/home/ishan/vscode-workspace/receipt-data-extraction/data/train/'
         else:
             self.base_path = '/home/ishan/vscode-workspace/receipt-data-extraction/data/test/'
-        
+
         self.base_files = []
-        assert len(os.listdir(self.base_path+self.img_path)) == len(os.listdir(self.base_path+self.entities_path)) == len(os.listdir(self.base_path+self.box_path))
+        assert len(os.listdir(self.base_path+self.img_path)) == len(os.listdir(
+            self.base_path+self.entities_path)) == len(os.listdir(self.base_path+self.box_path))
         for file in os.listdir(self.base_path+self.img_path):
             self.base_files.append(file.split('.')[0])
 
@@ -40,7 +42,8 @@ class PreprocessorSROIE:
             word_pointer = int(line[0])
             line_words = []
             for word in words:
-                line_words.append([[word_pointer, int(line[1]), word_pointer+per_word_width*len(word), int(line[7])], word, "OTHER"])
+                line_words.append([[word_pointer, int(
+                    line[1]), word_pointer+per_word_width*len(word), int(line[7])], word, "OTHER"])
                 word_pointer += per_word_width*len(word)+per_word_width
             lines.append(line_words)
         return lines
@@ -52,12 +55,12 @@ class PreprocessorSROIE:
             for line in lines:
                 text = " ".join([word[1] for word in line])
                 if k.upper() == "ADDRESS":
-                    if text in v and len(line)>2:
+                    if text in v and len(line) > 2:
                         for word in line:
                             word[2] = k.upper()
                 elif k.upper() == "COMPANY":
                     distance = jellyfish.jaro_distance(text, v)
-                    if distance > 0.8 and len(line)>2:
+                    if distance > 0.8 and len(line) > 2:
                         for word in line:
                             if word[1] in v:
                                 word[2] = k.upper()
@@ -68,7 +71,7 @@ class PreprocessorSROIE:
                             if word[1] in v:
                                 word[2] = k.upper()
 
-    def get_processed_data(self,lines):
+    def get_processed_data(self, lines):
         for line in lines:
             for word in line:
                 self.box_data.append(word[0])
@@ -78,26 +81,28 @@ class PreprocessorSROIE:
     def process(self):
         for file in tqdm(self.base_files):
             lines = self.process_box(self.base_path+self.box_path+file+'.txt')
-            self.process_entities(self.base_path+self.entities_path+file+'.txt', lines)
+            self.process_entities(
+                self.base_path+self.entities_path+file+'.txt', lines)
             self.get_processed_data(lines)
             if self.process_mode == 'train':
-                self.processed_data[file] = {
+                self.processed_data.append({
                     'img': self.base_path+self.img_path+file+'.jpg',
                     'box': self.box_data,
                     'words': self.word_data,
                     'labels': self.entities_data
-                }
+                })
             else:
-                self.processed_data[file] = {
+                self.processed_data.append({
                     'img': self.base_path+self.img_path+file+'.jpg',
                     'box': self.box_data,
                     'words': self.word_data,
-                }
+                })
             self.box_data = []
             self.word_data = []
             self.entities_data = []
         # open('processed_data.json', 'w').write(json.dumps(self.processed_data))
         return self.processed_data
+
 
 if __name__ == "__main__":
     preprocessor = PreprocessorSROIE()
