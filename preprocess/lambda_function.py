04ac9f5d492924dd65c37975bfb80fbb7da59555
Ishan Modi <54568147+ishan-modi@users.noreply.github.com>
2023-08-22 15:11:04 +0530

    Update lambda_function.py

--- preprocess/lambda_function.py
+++ preprocess/lambda_function.py
@@ -4,7 +4,7 @@ from preprocess_textract import PreProcessTextract
 def lambda_handler(event, context):
     preprocess = PreProcessTextract()
     preprocess.process()
-    print('TESTING CHANGES !!!')
+    print('TESTING CHANGES')
     return {
         'statusCode': 200,
         'body': json.dumps('Data Processed Successfully!')
